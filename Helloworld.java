3e7ef58b82a2a3263e33e1d9a092836b3217fca2
NareshKatam <naresh_katam@optum.com>
2015-03-16 17:29:15 +0530

    Commited

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,6 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Sample Github Code");
-		System.out.println("Welcome to Github-- Loaded from Local System");
+		System.out.println("Welcome to Github-- Loaded from Local System   ");
 	}
 }
